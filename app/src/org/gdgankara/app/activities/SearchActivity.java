49cefeb7dd2a57df51301544d94b85324d95ca13
andromedateam <orhunmertsimsek@gmail.com>
2013-03-16 13:28:34 +0200

    Search yavaş yavaş oluşturuluyor.

--- app/src/org/gdgankara/app/activities/SearchActivity.java
+++ app/src/org/gdgankara/app/activities/SearchActivity.java
@@ -1,6 +1,7 @@
 package org.gdgankara.app.activities;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.gdgankara.app.R;
 import org.gdgankara.app.R.layout;
@@ -11,19 +12,24 @@ import android.app.Activity;
 import android.text.Editable;
 import android.text.Html.TagHandler;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 public class SearchActivity extends Activity implements TextWatcher {
 
 	private AutoCompleteTextView searchText;
-	private ArrayList<String> tags;
+	private ArrayList<String> tags, searchResult;
 	private ImageView searchButton;
 	private org.gdgankara.app.io.TagHandler tagHand;
+	private String search;
+	private ListView searchList;
+	private ArrayAdapter<String> adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,11 @@ public class SearchActivity extends Activity implements TextWatcher {
 		
 		searchText = (AutoCompleteTextView) findViewById(R.id.searchText);
 		searchButton = (ImageView) findViewById(R.id.searchActivityButton);
-		tagHand = new org.gdgankara.app.io.TagHandler(this);
+		searchList = (ListView) findViewById(R.id.searchResultList);
 		
+		tagHand = new org.gdgankara.app.io.TagHandler(this);
+		searchResult = new ArrayList<String>();
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,searchResult);
 		
 		tags = tagHand.getTagList("tr");
 		
@@ -46,7 +55,11 @@ public class SearchActivity extends Activity implements TextWatcher {
 			
 			@Override
 			public void onClick(View arg0) {
-				
+				search = searchText.getText().toString();
+				if(search !=  "" && search != null){
+					adapter.add(search + " listeye eklendi");
+					searchList.setAdapter(adapter);
+				}
 			}
 		});
 	}
---
+++ app/src/org/gdgankara/app/search/Search.java
@@ -0,0 +1,19 @@
+package org.gdgankara.app.search;
+
+import java.util.ArrayList;
+
+public class Search {
+
+	private ArrayList<String> searchResult;
+	
+	/*
+	 * Bu fonksiyon çaðýrýldýðýnda gelen arama anahtarýna göre tüm oturumlar aranacak, eþleþenler ArrayList'e eklenecek.
+	 */
+	public void makeSearch(String searchKey){
+		
+		
+	}
+	public ArrayList<String> getResult(){
+		return this.searchResult;
+	}
+}
