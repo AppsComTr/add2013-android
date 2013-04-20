6fc151cdf20a94167107c8bcfa60b7946116e47e
andromedateam <orhunmertsimsek@gmail.com>
2013-04-20 20:35:52 +0300

    Geçici resimlerle Anasayfadaki resim geçişleri sağlandı.

--- app/src/org/gdgankara/app/activities/MainActivity.java
+++ app/src/org/gdgankara/app/activities/MainActivity.java
@@ -25,14 +25,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
 
-	private Button tweetWallButton,programButton,haritaButton,oturumButton,sponsorButton,favoriButton,konusmaciButton;
+	private Button tweetWallButton, programButton, haritaButton, oturumButton,
+			sponsorButton, favoriButton, konusmaciButton;
 	private ViewFlipper newsFlipper;
-	private ImageView araButton,tempImg;
+	private ImageView araButton, tempImg;
 	private String filepath;
 	private ArrayList<String> imagePaths;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,49 +44,65 @@ public class MainActivity extends Activity implements OnClickListener{
 		setUpButtons();
 		buttonsActive();
 		
-		
-		
-		Thread thread = new Thread()
-		{
-		    @Override
-		    public void run() {
-		        try {
-		            while(true) {
-		                sleep(1000);
-		                refreshView();
-		            }
-		        } catch (InterruptedException e) {
-		            e.printStackTrace();
-		        }
-		    }
+		tempImg = new ImageView(this);
+		tempImg.setImageResource(R.drawable.one);
+		newsFlipper.addView(tempImg);
+		tempImg = new ImageView(this);
+		tempImg.setImageResource(R.drawable.two);
+		newsFlipper.addView(tempImg);
+		tempImg = new ImageView(this);
+		tempImg.setImageResource(R.drawable.three);
+		newsFlipper.addView(tempImg);
+		tempImg = new ImageView(this);
+		tempImg.setImageResource(R.drawable.four);
+		newsFlipper.addView(tempImg);
+		tempImg = new ImageView(this);
+		tempImg.setImageResource(R.drawable.five);
+		newsFlipper.addView(tempImg);
+
+		newsFlipper.startFlipping();
+
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						sleep(1000);
+						refreshView();
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		};
 
 		thread.start();
-		
+
 	}
-	private void setUpButtons(){
+
+	private void setUpButtons() {
 		tweetWallButton = (Button) findViewById(R.id.tweetWall);
 		favoriButton = (Button) findViewById(R.id.favori_button);
-		haritaButton=(Button) findViewById(R.id.harita_button);
-		sponsorButton=(Button) findViewById(R.id.sponsor_button);
-		oturumButton=(Button) findViewById(R.id.oturum_button);
-		programButton=(Button) findViewById(R.id.program_button);
-		araButton = (ImageView)findViewById(R.id.search_button);
-		konusmaciButton=(Button)findViewById(R.id.speakers_button);
-		newsFlipper = (ViewFlipper)findViewById(R.id.highlights);
+		haritaButton = (Button) findViewById(R.id.harita_button);
+		sponsorButton = (Button) findViewById(R.id.sponsor_button);
+		oturumButton = (Button) findViewById(R.id.oturum_button);
+		programButton = (Button) findViewById(R.id.program_button);
+		araButton = (ImageView) findViewById(R.id.search_button);
+		konusmaciButton = (Button) findViewById(R.id.speakers_button);
+		newsFlipper = (ViewFlipper) findViewById(R.id.highlights);
 	}
-	
-	private void setHighligths()
-	{
-		
+
+	private void setHighligths() {
+
 	}
-	
-	private void refreshView(){
-//		imagePaths = new ArrayList<String>();
-//		imagePaths.add(getImagesFromWeb("https://twitter.com/images/resources/twitter-bird-light-bgs.png","downloadedFile.png"));
-//		Log.i("image path", imagePaths.get(0));
+
+	private void refreshView() {
+		// imagePaths = new ArrayList<String>();
+		// imagePaths.add(getImagesFromWeb("https://twitter.com/images/resources/twitter-bird-light-bgs.png","downloadedFile.png"));
+		// Log.i("image path", imagePaths.get(0));
 	}
-	private void buttonsActive(){
+
+	private void buttonsActive() {
 		tweetWallButton.setOnClickListener(this);
 		favoriButton.setOnClickListener(this);
 		haritaButton.setOnClickListener(this);
@@ -105,98 +122,94 @@ public class MainActivity extends Activity implements OnClickListener{
 
 	@Override
 	public void onClick(View arg0) {
-		Intent i=null;
-		
-		switch(arg0.getId()){
-		
+		Intent i = null;
+
+		switch (arg0.getId()) {
+
 		case R.id.tweetWall:
-			i = new Intent(MainActivity.this , TweetWallActivity.class);
+			i = new Intent(MainActivity.this, TweetWallActivity.class);
 			startActivity(i);
 			break;
-			
+
 		case R.id.program_button:
 			break;
-			
+
 		case R.id.harita_button:
 			i = new Intent(MainActivity.this, MyMapActivity.class);
 			startActivity(i);
 			break;
-			
+
 		case R.id.sponsor_button:
-			i = new Intent(MainActivity.this,SponsorListActivity.class);
+			i = new Intent(MainActivity.this, SponsorListActivity.class);
 			startActivity(i);
 			break;
-			
+
 		case R.id.oturum_button:
-			i = new Intent(MainActivity.this ,TagListActivity.class);
+			i = new Intent(MainActivity.this, TagListActivity.class);
 			startActivity(i);
 			break;
-			
+
 		case R.id.favori_button:
-			i = new Intent(MainActivity.this , FavoriteListActivity.class);
+			i = new Intent(MainActivity.this, FavoriteListActivity.class);
 			startActivity(i);
 			break;
-			
+
 		case R.id.speakers_button:
-			i = new Intent(MainActivity.this ,SpeakerListActivity.class);
+			i = new Intent(MainActivity.this, SpeakerListActivity.class);
 			startActivity(i);
 			break;
 		case R.id.search_button:
-			i = new Intent(MainActivity.this , SearchActivity.class);
+			i = new Intent(MainActivity.this, SearchActivity.class);
 			startActivity(i);
 			break;
-		
+
 		}
-		
-		
-//		startActivity(i);
-		
+
+		// startActivity(i);
+
 	}
-	
-	private String getImagesFromWeb(String urlString,String filename){
-		try
-		{   
-		  URL url = new URL(urlString);
-		  HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-		  urlConnection.setRequestMethod("GET");
-		  urlConnection.setDoOutput(true);                   
-		  urlConnection.connect();                  
-		  File SDCardRoot = Environment.getExternalStorageDirectory().getAbsoluteFile();
-		  Log.i("Yol",SDCardRoot.getAbsolutePath());
-		  Log.i("Local filename:",""+filename);
-		  File file = new File(SDCardRoot,filename);
-		  if(file.createNewFile())
-		  {
-		    file.createNewFile();
-		  }                 
-		  FileOutputStream fileOutput = new FileOutputStream(file);
-		  InputStream inputStream = urlConnection.getInputStream();
-		  int totalSize = urlConnection.getContentLength();
-		  int downloadedSize = 0;   
-		  byte[] buffer = new byte[1024];
-		  int bufferLength = 0;
-		  while ( (bufferLength = inputStream.read(buffer)) > 0 ) 
-		  {                 
-		    fileOutput.write(buffer, 0, bufferLength);                  
-		    downloadedSize += bufferLength;                 
-		    Log.i("Progress:","downloadedSize:"+downloadedSize+"totalSize:"+ totalSize) ;
-		  }             
-		  fileOutput.close();
-		  if(downloadedSize==totalSize) filepath=file.getPath();    
-		} 
-		catch (MalformedURLException e) 
-		{
-		  e.printStackTrace();
-		} 
-		catch (IOException e)
-		{
-		  filepath=null;
-		  e.printStackTrace();
+
+	private String getImagesFromWeb(String urlString, String filename) {
+		try {
+			URL url = new URL(urlString);
+			HttpURLConnection urlConnection = (HttpURLConnection) url
+					.openConnection();
+			urlConnection.setRequestMethod("GET");
+			urlConnection.setDoOutput(true);
+			urlConnection.connect();
+			File SDCardRoot = Environment.getExternalStorageDirectory()
+					.getAbsoluteFile();
+			Log.i("Yol", SDCardRoot.getAbsolutePath());
+			Log.i("Local filename:", "" + filename);
+			File file = new File(SDCardRoot, filename);
+			if (file.createNewFile()) {
+				file.createNewFile();
+			}
+			FileOutputStream fileOutput = new FileOutputStream(file);
+			InputStream inputStream = urlConnection.getInputStream();
+			int totalSize = urlConnection.getContentLength();
+			int downloadedSize = 0;
+			byte[] buffer = new byte[1024];
+			int bufferLength = 0;
+			while ((bufferLength = inputStream.read(buffer)) > 0) {
+				fileOutput.write(buffer, 0, bufferLength);
+				downloadedSize += bufferLength;
+				Log.i("Progress:", "downloadedSize:" + downloadedSize
+						+ "totalSize:" + totalSize);
+			}
+			fileOutput.close();
+			if (downloadedSize == totalSize)
+				filepath = file.getPath();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			filepath = null;
+			e.printStackTrace();
 		}
-		Log.i("filepath:"," "+filepath) ;
+		Log.i("filepath:", " " + filepath);
 		return filepath;
 	}
-	
+
 	private int getDimensionsOfScreen() {
 		DisplayMetrics metrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(metrics);
