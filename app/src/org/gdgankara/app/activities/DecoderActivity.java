8bb1f89a60a3019f2ffc3cbca1a4268d0a80353b
KURT <KURT@KURT-Bilgisayar>
2013-05-28 04:11:04 +0300

    QR Code add contact dışında bitirildi.

--- app/src/org/gdgankara/app/activities/DecoderActivity.java
+++ app/src/org/gdgankara/app/activities/DecoderActivity.java
@@ -2,13 +2,8 @@ package org.gdgankara.app.activities;
 
 
 import org.gdgankara.app.utils.Util;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.util.Log;
-import android.widget.Toast;
-
 import com.google.zxing.Result;
 import com.google.zxing.client.android.CaptureActivity;
 import com.google.zxing.client.android.result.ResultHandler;
@@ -20,8 +15,8 @@ public class DecoderActivity extends CaptureActivity {
 	public void handleDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
 		ResultHandler resultHandler = ResultHandlerFactory.makeResultHandler(
 				this, rawResult);
-		Log.i("Sedat","DecoderActivity icerisine girdi");
 		if(checkQRMessage(resultHandler.getResult().toString())){
+		
 			Intent intent = new Intent(this,EmployeeIdActivity.class);
 			intent.putExtra("SCAN_RESULT",resultHandler.getResult().toString() );
 			startActivity(intent);
@@ -35,7 +30,7 @@ public class DecoderActivity extends CaptureActivity {
 	
 	private boolean checkQRMessage(String message){
 		String[] temp=message.split("<#>");
-		if(temp.length==3){
+		if(temp.length==7){
 			return true;
 		}
 		return false;
--- app/src/org/gdgankara/app/activities/EmployeeIdActivity.java
+++ app/src/org/gdgankara/app/activities/EmployeeIdActivity.java
@@ -5,7 +5,12 @@ import org.gdgankara.app.listeners.TabListener;
 import org.gdgankara.app.utils.Util;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,6 +23,7 @@ public class EmployeeIdActivity extends Activity{
 	private String message;
 	private String[] temp;
 	private TextView text;
+	private ImageView imageview;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +46,38 @@ public class EmployeeIdActivity extends Activity{
 	}
 	
 	private void setUpViews() {
-		text=(TextView)findViewById(R.id.name_idpage);
-		text.setText(temp[0]);
-		text=(TextView)findViewById(R.id.email_idpage);
-		text.setText(temp[1]);
-		text=(TextView)findViewById(R.id.tel_idpage);
+		text=(TextView)findViewById(R.id.idpage_name);
+		text.setText(temp[0]+" "+temp[1]);
+		text=(TextView)findViewById(R.id.idpage_email);
 		text.setText(temp[2]);
+		text=(TextView)findViewById(R.id.idpage_website);
+		text.setText(Html.fromHtml("<a href=\""+temp[3]+"\">" +temp[3]+ "</a>"));
+		text.setMovementMethod(LinkMovementMethod.getInstance());
+		text=(TextView)findViewById(R.id.idpage_telephone);
+		text.setText(temp[4]);
+		text=(TextView)findViewById(R.id.idpage_company);
+		text.setText(temp[5]);
+		text=(TextView)findViewById(R.id.idpage_interested);
+		text.setText(temp[6]);
+		imageview=(ImageView)findViewById(R.id.add_contact_button);
+		imageview.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+					addContact();
+			}
+		});
+		if(Util.getDefaultLanguage().equals("tr")){
+			imageview.setImageDrawable(getResources().getDrawable(R.drawable.kisi_ekle));
+		}
+		else{
+			imageview.setImageDrawable(getResources().getDrawable(R.drawable.add_contact));
+		}
+	}
+	
+	private void addContact() {
+		Intent addContactIntent = new Intent(Intent.ACTION_INSERT);
+		startActivity(addContactIntent);
 	}
 
 	private void setActivityTheme(){
