70d2aeabec0099b3cce9ccaf4e2e2351044415e6
KURT <KURT@KURT-Bilgisayar>
2013-05-29 23:31:39 +0300

    Program Pager'ı ayağa kaldırılıyor

--- app/src/org/gdgankara/app/activities/ProgramActivity.java
+++ app/src/org/gdgankara/app/activities/ProgramActivity.java
@@ -4,14 +4,13 @@ import org.gdgankara.app.adapeters.ProgramFragmentAdapter;
 import org.gdgankara.app.listeners.TabListener;
 import org.gdgankara.app.utils.Util;
 import org.gdgankara.app.R;
-
 import com.viewpagerindicator.TitlePageIndicator;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class ProgramActivity extends FragmentActivity{ 
 	
@@ -29,15 +28,24 @@ public class ProgramActivity extends FragmentActivity{
 		setActivityTheme();
 		setContentView(R.layout.program);
 		setUpViews();
+		tabAktif();
+	}
+	
+	public void tabAktif(){
+		tabListener=new TabListener(this);
+		((ImageView)findViewById(R.id.search_button)).setOnClickListener(tabListener);	
+		((ImageView)findViewById(R.id.update_button)).setOnClickListener(tabListener);
+		((ImageView)findViewById(R.id.qr_decoder_button)).setOnClickListener(tabListener);	
+		
 	}
 
 	private void setUpViews() {
 		
-		program_adapter=new ProgramFragmentAdapter(getSupportFragmentManager());
-		
-		viewpager=(ViewPager)findViewById(R.id.program_pager);
+		program_adapter = new ProgramFragmentAdapter(getSupportFragmentManager());
+
+		viewpager = (ViewPager)findViewById(R.id.program_pager);
 		viewpager.setAdapter(program_adapter);
-		
+
 		title_indicator = (TitlePageIndicator)findViewById(R.id.title_indicator);
 		title_indicator.setViewPager(viewpager);
 		
--- app/src/org/gdgankara/app/adapeters/ProgramFragmentAdapter.java
+++ app/src/org/gdgankara/app/adapeters/ProgramFragmentAdapter.java
@@ -5,45 +5,46 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import com.viewpagerindicator.IconPagerAdapter;
+import org.gdgankara.app.TestFragment;
+import org.gdgankara.app.R;
 
-public class ProgramFragmentAdapter extends FragmentPagerAdapter implements IconPagerAdapter{
+public class ProgramFragmentAdapter extends FragmentPagerAdapter{
 	
-	private String[] tabs;
-	private Fragment[] fragments;
+	 private String[] CONTENT ;
+	 private int mCount;
 
-	public ProgramFragmentAdapter(FragmentManager fm) {
-		super(fm);
-		tabs=new String[2];
-		if(Util.getDefaultLanguage().equals("tr")){
-			tabs[0]="Cuma";
-			tabs[1]="Cumartesi";
-		}
-		else{
-			tabs[0]="Friday";
-			tabs[1]="Saturday";
-		}
-		
-	}
+	    public ProgramFragmentAdapter(FragmentManager fm) {
+	        super(fm);
+	        if(Util.getDefaultLanguage().equals("tr")){
+	        	CONTENT = new String[] { "Cuma", "Cumartesi", };
+	        }
+	        else{
+	        	CONTENT= new String[] { "Friday", "Saturday", };
+	        }
+	        mCount=CONTENT.length;
+	    }
 
-	@Override
-	public int getIconResId(int index) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	@Override
-    public CharSequence getPageTitle(int position) {
-      return tabs[position];
-    }
+	    @Override
+	    public Fragment getItem(int position) {
+	        return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+	    }
 
-	@Override
-	public Fragment getItem(int arg0) {
-		return new Fragment();
-	}
+	    @Override
+	    public int getCount() {
+	        return mCount;
+	    }
 
-	@Override
-	public int getCount() {
-		return 2;
-	}
+	    @Override
+	    public CharSequence getPageTitle(int position) {
+	      return CONTENT[position % CONTENT.length];
+	    }
+
+
+	    public void setCount(int count) {
+	        if (count > 0 && count <= 10) {
+	            mCount = count;
+	            notifyDataSetChanged();
+	        }
+	    }
 
 }
